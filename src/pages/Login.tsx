0483afb0c7205eb8f7622b768839425506bbfdda
abhi-2255 <abhinandpv820@gmail.com>
2025-08-24 23:42:14 +0530

    added Tanstack query

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,38 @@
+import { useNavigate } from "@tanstack/react-router"
 import { useState } from "react"
 
 const Login = () => {
 
     const [currentState, setCurrentState] = useState('Signup')
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        mobile: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    })
+
+    const onSubmitHandler = async (event) =>{
+        event.preventDefault();
+    }
     return (
         <div className="container mx-auto mt-10 flex justify-center ">
             <div className="flex flex-col justify-center items-center w-md text-gray-800 bg-amber-50 space-y-3 p-5">
                 <div className="inline-flex items-center ">
                     <h2 className="text-3xl mb-4">{currentState}</h2>
                 </div>
-                <form action="" className="flex flex-col justify-center items-center space-y-3">
-                    {currentState === 'Login' ? '' : <input type="text" className="px-3 py-2 border border-gray-800" placeholder="First name" />}
-                    {currentState === 'Login' ? '' : < input type="text" className="px-3 py-2 border border-gray-800" placeholder="Last name" />}
-                    {currentState === 'Login' ? '' : < input type="text" className="px-3 py-2 border border-gray-800" placeholder="Mobile Number" />}
-                    <input type="text" className="px-3 py-2 border border-gray-800" placeholder="Email" />
-                    <input type="text" className="px-3 py-2 border border-gray-800" placeholder="Password" />
-                    {currentState === 'Login' ? '' : <input type="text" className="px-3 py-2 border border-gray-800" placeholder="Confirm Password" />}
+                <form onSubmit={onSubmitHandler} className="flex flex-col justify-center items-center space-y-3">
+                    {currentState === 'Login' ? '' : (
+                        <>
+                            <input type="text" name="firstName" value={formData.firstName} className="px-3 py-2 border border-gray-800" placeholder="First name" />
+                            < input type="text" name="lastName" value={formData.lastName} className="px-3 py-2 border border-gray-800" placeholder="Last name" />
+                            < input type="text" name="mobile" value={formData.mobile} className="px-3 py-2 border border-gray-800" placeholder="Mobile Number" />
+                        </>
+                    )}
+                    <input type="text" name="email" value={formData.email} className="px-3 py-2 border border-gray-800" placeholder="Email" />
+                    <input type="text" name="password" value={formData.password} className="px-3 py-2 border border-gray-800" placeholder="Password" />
+                    {currentState === 'Login' ? '' : <input type="text" name="confirmPassword" value={formData.confirmPassword} className="px-3 py-2 border border-gray-800" placeholder="Confirm Password" />}
                     <div className="flex justify-evenly gap-2">
                         <p className="cursor-pointer text-sm">Forgot your password?</p>
                         {currentState === 'Login'
