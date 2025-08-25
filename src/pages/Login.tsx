c258069f909771fe6c956175e57519ec5ee812bb
abhi-2255 <abhinandpv820@gmail.com>
2025-08-25 23:53:35 +0530

    modified codes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -18,21 +18,21 @@ const Login = () => {
     }
     return (
         <div className="container mx-auto mt-10 flex justify-center ">
-            <div className="flex flex-col justify-center items-center w-md text-gray-800 bg-amber-50 space-y-3 p-5">
+            <div className="flex flex-col justify-center items-center w-xs text-gray-800 bg-amber-50 space-y-3 p-5">
                 <div className="inline-flex items-center ">
-                    <h2 className="text-3xl mb-4">{currentState}</h2>
+                    <h2 className="text-3xl mb-2">{currentState}</h2>
                 </div>
-                <form onSubmit={onSubmitHandler} className="flex flex-col justify-center items-center space-y-3">
+                <form onSubmit={onSubmitHandler} className="flex flex-col justify-center items-center w-full space-y-3">
                     {currentState === 'Login' ? '' : (
                         <>
-                            <input type="text" name="firstName" value={formData.firstName} className="px-3 py-2 border border-gray-800" placeholder="First name" />
-                            < input type="text" name="lastName" value={formData.lastName} className="px-3 py-2 border border-gray-800" placeholder="Last name" />
-                            < input type="text" name="mobile" value={formData.mobile} className="px-3 py-2 border border-gray-800" placeholder="Mobile Number" />
+                            <input type="text" name="firstName" value={formData.firstName} className="px-3 py-2 w-[90%]  border border-gray-800" placeholder="First name" />
+                            < input type="text" name="lastName" value={formData.lastName} className="px-3 py-2 w-[90%]  border border-gray-800" placeholder="Last name" />
+                            < input type="text" name="mobile" value={formData.mobile} className="px-3 py-2 w-[90%]  border border-gray-800" placeholder="Mobile Number" />
                         </>
                     )}
-                    <input type="text" name="email" value={formData.email} className="px-3 py-2 border border-gray-800" placeholder="Email" />
-                    <input type="text" name="password" value={formData.password} className="px-3 py-2 border border-gray-800" placeholder="Password" />
-                    {currentState === 'Login' ? '' : <input type="text" name="confirmPassword" value={formData.confirmPassword} className="px-3 py-2 border border-gray-800" placeholder="Confirm Password" />}
+                    <input type="text" name="email" value={formData.email} className="px-3 py-2 w-[90%]  border border-gray-800" placeholder="Email" />
+                    <input type="text" name="password" value={formData.password} className="px-3 py-2 w-[90%]  border border-gray-800" placeholder="Password" />
+                    {currentState === 'Login' ? '' : <input type="text" name="confirmPassword" value={formData.confirmPassword} className="px-3 py-2 w-[90%]  border border-gray-800" placeholder="Confirm Password" />}
                     <div className="flex justify-evenly gap-2">
                         <p className="cursor-pointer text-sm">Forgot your password?</p>
                         {currentState === 'Login'
