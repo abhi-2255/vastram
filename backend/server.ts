f4b68348bfc3ef562637906a4eb7edf792fe352c
abhi-2255 <abhinandpv820@gmail.com>
2025-09-08 23:30:22 +0530

    modified codes

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,8 @@
-const { default: mongoose } = require("mongoose")
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import authRoutes from "./routes/auth"
+import productRoutes from "./routes/ProductRoutes"
 
 const app = express()
 app.use(cors())
@@ -12,5 +16,6 @@ mongoose.connect("mongodb://localhost:27017/vastram",{
 .catch(err=> console.error(err))
 
 app.use("/api/auth",authRoutes)
+app.use("/api/products", productRoutes)
 
 app.listen(5000,()=>console.log("Server running"))
