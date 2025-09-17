77c1d4e46ed64a6f6100fc62e755f7e2f87850e2
abhi-2255 <abhinandpv820@gmail.com>
2025-09-18 01:52:36 +0530

    modified codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 
+import { useState } from 'react'
 import Home from './pages/Home'
 import Login from './pages/LoginSignup'
 import OtpForm from './pages/Otp'
 
 export default function App() {
+
+  const [users. setUsers] = useState([])
+
+  const fetchUsers = async()=>{
+    const res = await fetch("http://localhost:5000/api/users",{
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    const data = await res.json()
+    setUsers(data)
+  }
 
   return (
     <>
